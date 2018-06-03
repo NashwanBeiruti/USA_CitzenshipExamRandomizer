8b7da02490b898630270530f09924ffdcdb3935c
NashwanBeiruti <38735047+NashwanBeiruti@users.noreply.github.com>
2018-06-02 21:09:54 -0400

    Question

---
+++ src/Question.java
@@ -0,0 +1,11 @@
+public class Question {
+   int number;
+   String question;
+   boolean right;
+   
+   public Question (int number, String question) {
+      this.number = number;
+      this.question = question;
+   }//constructor
+   
+}//class
